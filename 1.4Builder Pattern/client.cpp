8b1d8ec13fe1cf105ee8a8d96ca1daee4ef3d77c
ImreW <94045136+imrewang@users.noreply.github.com>
2022-12-17 23:06:43 +0800

    Rename Builder Pattern/client.cpp to 1.4Builder Pattern/client.cpp

--- 1.4Builder Pattern/client.cpp
+++ 1.4Builder Pattern/client.cpp
@@ -1,7 +1,7 @@
 #include"builder.h"
 
 int main() {
-	//client�û�
+	//clientÓĂť§
 	Director* car_director = new Director();
 	Car* bm = car_director->construct_car(new BMBuilder());
 	Car* bz = car_director->construct_car(new BZBuilder());
